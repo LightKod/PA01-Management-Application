b77b17232e29e3d466e7df8165870c1344611693
NghiaLe128 <nghianguyenlethanh1282003@gmail.com>
2024-02-27 13:08:41 +0700

    update menu, navbar, slider

---
+++ PA01-Management-Application/PA01-Management-Application/MVVM/Commands/UpdateViewCommand.cs
@@ -0,0 +1,36 @@
+﻿using PA01_Management_Application.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PA01_Management_Application.MVVM.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter.ToString() == "Home")
+            {
+                viewModel.SelectedViewModel = new HomeViewModel();
+            }
+            
+        }
+    }
+}
