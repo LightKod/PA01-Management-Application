debafbe49e075f76903404497261f21f7d80f434
PICKLE\hoang <21127092@student.hcmus.edu.vn>
2024-03-09 11:38:14 +0700

    added new app's layout, implemented simple search bar with pagination

---
+++ PA01-Management-Application/PA01-Management-Application/MVVM/ViewModel/AppWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using PA01_Management_Application.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA01_Management_Application.MVVM.ViewModel
+{
+    class AppWindowViewModel : ObservableObject
+    {
+        //public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand SearchViewCommand { get; set; }
+
+        //public HomeViewModel HomeVM { get; set; }
+
+        public SearchViewModel SearchVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AppWindowViewModel()
+        {
+            //HomeVM = new HomeViewModel();
+            SearchVM = new SearchViewModel();
+            CurrentView = SearchVM;
+
+            /*HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });*/
+
+            SearchViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SearchVM;
+            });
+        }
+    }
+}
