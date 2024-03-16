81e37d540a2efef3fcad655e4d0da1aa8b80764e
Tran-Minh-Khoa <99416417+Tran-Minh-Khoa@users.noreply.github.com>
2024-03-17 01:14:10 +0700

    Booking-Screening-Time

---
+++ PA01-Management-Application/PA01-Management-Application/MVVM/View/BookingScreeningTime.xaml.cs
@@ -0,0 +1,106 @@
+﻿using PA01_Management_Application.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PA01_Management_Application.MVVM.View
+{
+
+    /// <summary>
+    /// Interaction logic for BookingScreeningTime.xaml
+    /// </summary>
+    public partial class BookingScreeningTime : UserControl
+    {
+        private bool isButtonPressed = false;
+        Button DateClickedButton = null;
+        Button CityClickedButton = null;
+        public class DateInfo
+        {
+            public string Day { get; set; }
+            public string DayOfWeek { get; set; }
+            public string Month { get; set; }
+        }
+        public class CinemaDetailsViewModel
+        {
+            public string CinemaName { get; set; }
+            public string TheaterName { get; set; }
+            public ObservableCollection<string> TimeButtons { get; set; }
+        }
+        BookingScreeningTimeViewModel ViewModel;
+        public BookingScreeningTime()
+        {
+            InitializeComponent();
+            ViewModel = new BookingScreeningTimeViewModel();
+            DataContext = ViewModel;
+        }
+
+        private void BtnDate_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (DateClickedButton != null)
+            {
+                DateClickedButton.Foreground = Brushes.Black; // Đặt màu chữ của nút về màu ban đầu
+                Border otherBorder = DateClickedButton.Template.FindName("PART_Border", DateClickedButton) as Border;
+                if (otherBorder != null)
+                {
+                    otherBorder.Background = Brushes.Transparent; // Đặt màu nền của border về màu ban đầu
+                }
+            }
+            DateClickedButton = sender as Button;
+
+
+            if (DateClickedButton != null)
+            {
+                // Thiết lập màu của nút được click
+                DateClickedButton.Foreground = Brushes.Black;
+                Border border = DateClickedButton.Template.FindName("PART_Border", DateClickedButton) as Border;
+                if (border != null)
+                {
+                    border.Background = Brushes.LightGray; // Đặt màu của border
+                }
+
+            }
+        }
+        private void BtnCity_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (CityClickedButton != null)
+            {
+                CityClickedButton.Foreground = Brushes.Black; // Đặt màu chữ của nút về màu ban đầu
+                Border otherBorder = CityClickedButton.Template.FindName("CityBorder", CityClickedButton) as Border;
+                if (otherBorder != null)
+                {
+                    otherBorder.Background = Brushes.Transparent; // Đặt màu nền của border về màu ban đầu
+                }
+            }
+            CityClickedButton = sender as Button;
+
+
+            if (CityClickedButton != null)
+            {
+                // Thiết lập màu của nút được click
+                CityClickedButton.Foreground = Brushes.Black;
+                Border border = CityClickedButton.Template.FindName("CityBorder", CityClickedButton) as Border;
+                if (border != null)
+                {
+                    border.Background = Brushes.LightGray; // Đặt màu của border
+                }
+
+            }
+        }
+    }
+}
---
+++ PA01-Management-Application/PA01-Management-Application/MVVM/ViewModel/BookingScreeningTimeViewModel.cs
@@ -0,0 +1,135 @@
+﻿using PA01_Management_Application.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static PA01_Management_Application.MVVM.View.BookingScreeningTime;
+
+namespace PA01_Management_Application.MVVM.ViewModel
+{
+    internal class BookingScreeningTimeViewModel : ObservableObject
+    {
+        private ObservableCollection<string> _cityList;
+        public ObservableCollection<string> CityList
+        {
+            get { return _cityList; }
+            set
+            {
+                _cityList = value;
+                OnPropertyChanged(nameof(CityList));
+            }
+        }
+
+        private ObservableCollection<DateInfo> _dateList;
+        public ObservableCollection<DateInfo> DateList
+        {
+            get { return _dateList; }
+            set
+            {
+                _dateList = value;
+                OnPropertyChanged(nameof(DateList));
+            }
+        }
+
+        private ObservableCollection<CinemaDetailsViewModel> _cinemaDetailsCollection;
+        public ObservableCollection<CinemaDetailsViewModel> CinemaDetailsCollection
+        {
+            get { return _cinemaDetailsCollection; }
+            set
+            {
+                _cinemaDetailsCollection = value;
+                OnPropertyChanged(nameof(CinemaDetailsCollection));
+            }
+        }
+        private DateInfo _selectedDate;
+        public DateInfo SelectedDate
+        {
+            get { return _selectedDate; }
+            set
+            {
+                _selectedDate = value;
+                OnPropertyChanged(nameof(SelectedDate));
+            }
+        }
+
+        private CinemaDetailsViewModel _selectedCinemaDetailsViewModel;
+        public CinemaDetailsViewModel SelectedCinemaDetailsViewModel
+        {
+            get { return _selectedCinemaDetailsViewModel; }
+            set
+            {
+                _selectedCinemaDetailsViewModel = value;
+                OnPropertyChanged(nameof(SelectedCinemaDetailsViewModel));
+            }
+        }
+        public RelayCommand DateButtonClickCommand { get; private set; }
+        public RelayCommand CinemaDetailsClickCommand { get; set; }
+
+        public BookingScreeningTimeViewModel()
+        {
+            // Khởi tạo CityList
+            CityList = new ObservableCollection<string>
+            {
+                "Hồ Chí Minh",
+                "Hà Nội",
+                "Đà Nẵng",
+                "Cần Thơ",
+                "Đồng Nai"
+            };
+
+            // Khởi tạo DateList
+            DateList = new ObservableCollection<DateInfo>
+            {
+                new DateInfo { Day = "15", DayOfWeek = "Mon", Month = "03" },
+                new DateInfo { Day = "25", DayOfWeek = "Tues", Month = "02" },
+                // Thêm các đối tượng dữ liệu khác nếu cần
+            };
+
+            // Khởi tạo CinemaDetailsCollection
+            CinemaDetailsCollection = new ObservableCollection<CinemaDetailsViewModel>();
+
+            // Thêm dữ liệu vào CinemaDetailsCollection
+            var cinemaDetails = new CinemaDetailsViewModel
+            {
+                CinemaName = "CGV Aeon Tân Phú",
+                TheaterName = "Rạp 27D",
+                TimeButtons = new ObservableCollection<string>
+                {
+                    "17:20 PM",
+                    "17:20 PM",
+                    // Thêm các giá trị thời gian khác nếu cần
+                }
+            };
+
+            CinemaDetailsCollection.Add(cinemaDetails);
+            CinemaDetailsCollection.Add(cinemaDetails);
+
+            DateButtonClickCommand = new RelayCommand(ExecuteDateButtonClick);
+            CinemaDetailsClickCommand = new RelayCommand(ExecuteCinemaDetailsClick);
+
+
+        }
+        private void ExecuteDateButtonClick(object parameter)
+        {
+            // Xử lý logic khi DateButton được click
+            SelectedDate = parameter as DateInfo;
+            Debug.WriteLine(SelectedDate.Day.ToString());
+        }
+        private void ExecuteCinemaDetailsClick(object parameter)
+        {
+            // Xử lý logic khi CinemaDetailsViewModel được click
+            // Sử dụng biến cinemaDetails ở đây để thực hiện các thao tác cần thiết
+            Debug.WriteLine("aaaaa");
+
+            SelectedCinemaDetailsViewModel = parameter as CinemaDetailsViewModel;
+            Debug.WriteLine(SelectedCinemaDetailsViewModel.TheaterName.ToString());
+
+        }
+
+
+    }
+}
