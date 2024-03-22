646cd2a2eb0eec2eaac7f4e55627861013896384
Tran-Minh-Khoa <99416417+Tran-Minh-Khoa@users.noreply.github.com>
2024-03-22 15:48:22 +0700

    report service

--- PA01-Management-Application/PA01-Management-Application/MVVM/Models/MovieManagementContext.cs
+++ PA01-Management-Application/PA01-Management-Application/MVVM/Models/MovieManagementContext.cs
@@ -37,7 +37,7 @@ public partial class MovieManagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-P1GE06K; Trusted_Connection=Yes; Initial Catalog=MovieManagement; TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-1P79QFBT; Trusted_Connection=Yes; Initial Catalog=MovieManagement; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -74,6 +74,9 @@ public partial class MovieManagementContext : DbContext
             entity.ToTable("bookingFood");
 
             entity.Property(e => e.BookingId).HasColumnName("booking_id");
+            entity.Property(e => e.BookingDate)
+               .HasColumnType("datetime")
+               .HasColumnName("booking_date");
             entity.Property(e => e.Price).HasColumnName("price");
             entity.Property(e => e.ScheduleId).HasColumnName("schedule_id");
             entity.Property(e => e.UserId).HasColumnName("user_id");
--- PA01-Management-Application/PA01-Management-Application/MVVM/ViewModel/Service/ReportService.cs
+++ PA01-Management-Application/PA01-Management-Application/MVVM/ViewModel/Service/ReportService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PA01_Management_Application.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,33 @@ namespace PA01_Management_Application.MVVM.ViewModel.Service
         //lấy giá Booking vé  date to date
         //lấy giá booking food date to date
 
+        private readonly MovieManagementContext _context = new MovieManagementContext();
+
+        // Lấy danh sách các booking của một bộ phim trong khoảng thời gian từ dateStart đến dateEnd, và trả về danh sách các đối tượng { date: price: }
+        public async Task<List<(DateTime? date, double? price)>> GetBookingsByMovieIdAndDateRangeAsync(int movieId, DateTime dateStart, DateTime dateEnd)
+        {
+            var bookings = _context.Bookings
+                .Where(b => b.Schedule != null && b.Schedule.MovieId == movieId && b.BookingDate >= dateStart && b.BookingDate <= dateEnd)
+                .GroupBy(b => b.BookingDate)
+                .Select(group => new { date = group.Key, price = group.Sum(b => b.Price) })
+                .ToList();
+            //Debug.WriteLine(bookings);
+            //var bookings2 = _context.Bookings.Where(b => b.Schedule != null && b.Schedule.MovieId == movieId && b.BookingDate >= dateStart && b.BookingDate <= dateEnd).ToList();
+            var foodBookings = _context.BookingFoods
+              .Where(bf => bf.Schedule != null && bf.Schedule.MovieId == movieId && bf.BookingDate >= dateStart && bf.BookingDate <= dateEnd)
+              .GroupBy(bf => bf.BookingDate)
+              .Select(group => new { date = group.Key, price = group.Sum(bf => bf.Price) })
+              .ToList();
+
+            var combinedBookings = bookings
+                .Concat(foodBookings)
+                .GroupBy(b => b.date)
+                .Select(group => new { date = group.Key, price = group.Sum(b => b.price) })
+                .ToList();
+
+            return combinedBookings.Select(b => (b.date, b.price)).ToList();
+            //return bookings.Select(b => (b.date, b.price)).ToList();
+        }
 
 
 
